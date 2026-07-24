c546952bb4f087fa62dd8746228c7868259f1e3e
NZN-123 <jngjn02@gmail.com>
2026-07-24 17:50:10 +0900

    feat: 개발 환경 설정 및 메인 페이지 초기 구성

---
+++ src/main/java/org/example/fatdogai2/controller/MainController.java
@@ -0,0 +1,18 @@
+package org.example.fatdogai2.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping
+
+public class MainController {
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+    <title>FatDogAI2</title>
+</head>
+<body>
+<h1>FatDogAI2</h1>
+</body>
+</html>
