6af8a8d3a73a61fbbccb796ae578008633b7affa
NZN-123 <jngjn02@gmail.com>
2026-07-24 17:33:56 +0900

    Initial commit

---
+++ src/test/java/org/example/fatdogai2/FatDogAi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fatdogai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatDogAi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
